9e2936c8df0cdd5650418a0bf54f6cd6c645c477
Filip Golewski <f.golewski@gmail.com>
2020-01-19 11:04:45 +0100

    Modified CircleCI configuration and npm script for yarn test

--- src/test.js
+++ src/test.js
@@ -1,3 +1,8 @@
+
+console.log('\x1b[33m%s\x1b[0m', "Welcome to test script for a package.");
+console.info("%cStarting a test", "color:blue");
+console.log('%cTHREE %cTWO %cONE', 'color:blue;border:1px solid black', 'color:black;border:1px solid black', 'color:black;border:1px solid black');
+
 let ean = require('./index.js');
 let code;
 code = ean.ean13.generate("09000001");
@@ -5,3 +10,6 @@ console.log("code: " + code + " check: " + ean.ean13.check(code));
 console.log(ean.ean13.generate(123));
 console.log(ean.ean13.check("1236"));
 console.log(ean.ean13.check("1237"));
+console.log("")
+
+
